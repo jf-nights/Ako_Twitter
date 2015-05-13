a373d1bb0e433ac84db6dd6775fe6befde475363
jf712@ring <jf712@kmc.gr.jp>
2015-05-13 17:37:50 +0900

    mongoンゴwwww

--- bot.rb
+++ bot.rb
@@ -2,11 +2,20 @@ Bundler.require
 
 require_relative './ako'
 ako = Ako.new
+
+# mongo
+mongo_client = Mongo::Client.new(['127.0.0.1:27272'], :database => 'twitter')
+db = mongo_client.database
+collection = db[:ako]
+
 warn "system start!!!"
 
 ako.stream_client.user do |object|
+  doc = nil
   case object
   when Twitter::Tweet
+    doc = { :status => object.to_h }
     ako.recieve(object)
   end
+  collection.insert_one(doc) unless doc.nil?
 end
--- cron.rb
+++ cron.rb
@@ -1,13 +1,17 @@
 Bundler.require
 require_relative './lib/twitter_client'
 require_relative './lib/markov2'
+require 'pp'
 
 twitter_client = TwitterClient.new('ako').client
 markov = Markov.new
+mongo = Mongo::Client.new(['127.0.0.1:27272'], :database => 'twitter')
+db = mongo.database
+coll = db[:ako]
 
 tweets = []
-open("./data/timeline.txt", "r").each do |line|
-    tweets << line.chomp
+coll.find.each do |row|
+  tweets << row[:status][:text]
 end
 
 tweet = markov.generate(tweets)

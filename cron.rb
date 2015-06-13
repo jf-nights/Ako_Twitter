50dc263df1901430329d5137b30f7eaa7503c275
jf712@ring <jf712@kmc.gr.jp>
2015-06-13 14:47:23 +0900

    0分と30分は最近のTLから

--- cron.rb
+++ cron.rb
@@ -5,14 +5,26 @@ require 'pp'
 
 twitter_client = TwitterClient.new('ako').client
 markov = Markov.new
-mongo = Mongo::Client.new(['127.0.0.1:27272'], :database => 'twitter')
-db = mongo.database
-coll = db[:ako]
-
+# マルコフするもとになるtweetの配列
 tweets = []
-coll.find.each do |row|
-  tweets << row[:status][:text]
-end
+# updateするtweet
+tweet = ''
 
+# 0分,30分は最近のTLから作る
+if Time.now.min == 0 || Time.now.min == 30
+  recent_tl = twitter_client.home_timeline(:count => 200)
+  recent_tl.each do |status|
+    tweets << status.text
+  end
+# 15分,45分は全部から
+else
+  mongo = Mongo::Client.new(['127.0.0.1:27272'], :database => 'twitter')
+  db = mongo.database
+  coll = db[:ako]
+
+  coll.find.each do |row|
+    tweets << row[:status][:text]
+  end
+end
 tweet = markov.generate(tweets)
 twitter_client.update(tweet)

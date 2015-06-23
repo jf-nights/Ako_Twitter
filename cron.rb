dd0e0ee3be88d826982087a5e13b0ce015beddc7
jf712@ring <jf712@kmc.gr.jp>
2015-06-23 17:33:33 +0900

    mongoの最新200件

--- cron.rb
+++ cron.rb
@@ -1,7 +1,6 @@
 Bundler.require
 require_relative './lib/twitter_client'
 require_relative './lib/markov2'
-require 'pp'
 
 twitter_client = TwitterClient.new('ako').client
 markov = Markov.new
@@ -9,19 +8,18 @@ markov = Markov.new
 tweets = []
 # updateするtweet
 tweet = ''
+# Mongo
+mongo = Mongo::Client.new(['127.0.0.1:27272'], :database => 'twitter')
+db = mongo.database
+coll = db[:ako]
 
 # 0分,30分は最近のTLから作る
 if Time.now.min == 0 || Time.now.min == 30
-  recent_tl = twitter_client.home_timeline(:count => 200)
-  recent_tl.each do |status|
-    tweets << status.text
+  coll.find.sort(:_id => -1).limit(200).each do |row|
+    tweets << row[:status][:text]
   end
 # 15分,45分は全部から
 else
-  mongo = Mongo::Client.new(['127.0.0.1:27272'], :database => 'twitter')
-  db = mongo.database
-  coll = db[:ako]
-
   coll.find.each do |row|
     tweets << row[:status][:text]
   end
